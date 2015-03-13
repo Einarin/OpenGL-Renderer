ef6f53227a27e4e8bc3d59c8721bb281a89d95e2
Johnny Dickinson <kc0itv@gmail.com>
2015-03-12 23:35:50 -0400

    added missing files needed by assimp

---
+++ assimp-3.1.1/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Johnny/Programs/Game/assimp-3.1.1/contrib/zlib
+# Build directory: C:/Users/Johnny/Programs/Game/assimp-3.1.1/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
